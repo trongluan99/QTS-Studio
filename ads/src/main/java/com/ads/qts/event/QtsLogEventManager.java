cfe0e242e0f4b0f86e3e1adc834a482d68afcdf4
Luan Dinh <trongluan8686@gmail.com>
2024-11-22 10:50:13 +0700

    Open Log revenue Facebook 30/08

--- ads/src/main/java/com/ads/qts/event/QtsLogEventManager.java
+++ ads/src/main/java/com/ads/qts/event/QtsLogEventManager.java
@@ -26,8 +26,8 @@ public class QtsLogEventManager {
         logEventWithAds(context, (float) adValue.getValueMicros(), adValue.getPrecisionType(), adUnitId, mediationAdapterClassName, QtsAdConfig.PROVIDER_ADMOB);
         QtsAdjust.pushTrackEventAdmob(adValue);
         // Log revenue Facebook 30/08
-        /*float value = adValue.getValueMicros() * 1.0f / 1000000 * 25000;
-        AppEventsLogger.newLogger(context).logPurchase(BigDecimal.valueOf(value), Currency.getInstance("VND"));*/
+        float value = adValue.getValueMicros() * 1.0f / 1000000 * 25000;
+        AppEventsLogger.newLogger(context).logPurchase(BigDecimal.valueOf(value), Currency.getInstance("VND"));
     }
 
     public static void logPaidAdjustWithToken(AdValue adValue, String adUnitId, String token) {
